a36f3d53168bae5c914e62b4cd54d07030e5293f
David Ayala and Katy Henning <katy@>
2016-09-20 11:43:48 -0700

    link tasks to specific categories

--- app/app.php
+++ app/app.php
@@ -1,4 +1,6 @@
 <?php
+
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
     require_once __DIR__."/../src/Category.php";
